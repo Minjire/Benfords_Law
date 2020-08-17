2288a078ca42f001c9d95559b2b71879d801a120
Minjire <minjirekelvin@gmail.com>
2020-08-17 20:52:40 +0300

    Redesign code into Functions

--- main.py
+++ main.py
@@ -9,46 +9,41 @@ fields = ["id", "base_price", "assessed_value"]
 df = pd.read_csv('tb_vehicle_valuation.csv', usecols=fields)
 print(df.head())
 
+
 # %% processing
-# drop columns with zeros
-df = df[(df != 0).all(1)]
-df = df.astype('str')
-df['bp_first_digit'] = df.base_price.str.extract('(\d)')
-df['bp_second_digit'] = df.base_price.str.extract('(\d\d)')
-df['bp_third_digit'] = df.base_price.str.extract('(\d\d\d)')
-df['av_first_digit'] = df.assessed_value.str.extract('(\d)')
-df['av_second_digit'] = df.assessed_value.str.extract('(\d\d)')
-df['av_third_digit'] = df.assessed_value.str.extract('(\d\d\d)')
+def pre_processing(data_df):
+    # drop columns with zeros
+    data_df = data_df[(data_df != 0).all(1)]
+    data_df = data_df.astype('str')
+    data_df['bp_first_digit'] = data_df.base_price.str.extract('(\d)')
+    data_df['bp_second_digit'] = data_df.base_price.str.extract('(\d\d)')
+    data_df['bp_third_digit'] = data_df.base_price.str.extract('(\d\d\d)')
+    data_df['av_first_digit'] = data_df.assessed_value.str.extract('(\d)')
+    data_df['av_second_digit'] = data_df.assessed_value.str.extract('(\d\d)')
+    data_df['av_third_digit'] = data_df.assessed_value.str.extract('(\d\d\d)')
 
-# drop nan columns
-df.dropna(axis=0, inplace=True)
+    # drop nan columns
+    data_df.dropna(axis=0, inplace=True)
 
-# replace column with 2nd digit
-df.bp_second_digit = df.bp_second_digit.astype('int') % 10
-df.av_second_digit = df.av_second_digit.astype('int') % 10
-df.bp_third_digit = df.bp_third_digit.astype('int') % 10
-df.av_third_digit = df.av_third_digit.astype('int') % 10
+    # replace column with 2nd digit
+    data_df.bp_second_digit = data_df.bp_second_digit.astype('int') % 10
+    data_df.av_second_digit = data_df.av_second_digit.astype('int') % 10
+    data_df.bp_third_digit = data_df.bp_third_digit.astype('int') % 10
+    data_df.av_third_digit = data_df.av_third_digit.astype('int') % 10
 
-print(df.head())
+    print(data_df.head())
+    data_df = data_df[['bp_first_digit', 'bp_second_digit', 'bp_third_digit', 'av_first_digit',
+                       'av_second_digit', 'av_third_digit']].astype('int').reset_index(drop=True)
+    return data_df
+
 
 # %% Plot Columns
-# convert columns to int
-df_digits = df[['bp_first_digit', 'bp_second_digit', 'bp_third_digit', 'av_first_digit',
-                'av_second_digit', 'av_third_digit']].astype('int').reset_index(drop=True)
+def plotting(df, col, i, j, axs):
+    # get length of dataframe
+    length, width = df.shape
 
-# get length of dataframe
-length, width = df_digits.shape
-
-columns1 = np.array(df_digits.columns[:3])
-columns2 = np.array(df_digits.columns[3:])
-arr = [columns1, columns2]
-
-fig, axs = plt.subplots(2, 3, figsize=(15, 15))
-
-
-def plotting(col, i, j):
     plt.sca(axs[i, j])
-    df_digits[col].value_counts().apply(lambda x: 100 * x / length) \
+    df[col].value_counts().apply(lambda x: 100 * x / length) \
         .plot(kind='bar', title=col)
     axs[i, j].yaxis.set_major_formatter(mtick.PercentFormatter())
     # set individual bar labels using above list
@@ -57,18 +52,46 @@ def plotting(col, i, j):
         axs[i, j].text(k.get_x(), k.get_height() + 0.5, round(k.get_height(), 2), color='green')
 
 
-for i in range(len(arr)):
-    for j in range(len(arr[i])):
-        print(f'{i}, {j}')
-        plotting(arr[i][j], i, j)
+def preplot(df_digits, name):
+    # convert columns to int
+    df_digits = pre_processing(df_digits)
 
-plt.savefig('Initial Benford.png', bbox_inches="tight")
+    fig, axs = plt.subplots(2, 3, figsize=(15, 15))
+
+    columns1 = np.array(df_digits.columns[:3])
+    columns2 = np.array(df_digits.columns[3:])
+    arr = [columns1, columns2]
+    print(f"Array: {arr}")
+
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print(f'{i}, {j}')
+            plotting(df_digits, arr[i][j], i, j, axs)
+
+    plt.show()
+    plt.savefig(name, bbox_inches="tight")
+
 
 # %%
-columns1 = np.array(df_digits.columns[:3])
-columns2 = np.array(df_digits.columns[3:])
-arr = [columns1, columns2]
-print(len(arr))
+preplot(df, 'Initial Benford.png')
+
+# %% Refined implementation
+print(f"Original Dataframe row count: {len(df.index)}")
+base_price = df[(df[['base_price']] != 0).all(1)]
+print(base_price.head())
+print(f"Row count of Dataframe without base price 'zero' values: {len(base_price.index)}")
+print(f"Dropped 'zero' rows in Dataframe: {len(df.index) - len(base_price.index)}")
+# assessed_value = df[df[['assessed_value']].eq(0).all(1)]
+assessed_value = df[(df[['assessed_value']] != 0).all(1)]
+print(f"Row count of Dataframe without assessed price 'zero' values: {len(assessed_value.index)}")
+print(f"Dropped 'zero' rows in Dataframe: {len(df.index) - len(assessed_value.index)}")
+
+# %% PLot new dataframe
+# df without zeros in base_price column
+preplot(base_price, "No Base Price 'Zero' Values.png")
+
+# %%
+# print(base_price.head())
 
 # print(df.isna().sum())
 # print(df.dtypes)
